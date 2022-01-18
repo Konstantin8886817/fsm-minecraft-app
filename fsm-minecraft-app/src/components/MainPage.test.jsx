83ff872ad54aeaeb03fc3383bd7d9d8a0821e33d
Konstantin <konstantin@ownbackup.com>
2022-01-18 15:22:22 +0200

    added tests

--- fsm-minecraft-app/src/components/MainPage.test.jsx
+++ fsm-minecraft-app/src/components/MainPage.test.jsx
@@ -1,12 +1,34 @@
 import '@testing-library/jest-dom'
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import MainPage from './MainPage'
 
-describe('MainPage component unit tests', () => {
-    test('Renders without crashing', () => {
-        const {getByText} = render(<MainPage />)
+const MOCK_BUTTON_TEXT = 'Smelt'
 
-        expect(getByText('Sand')).toBeInTheDocument();
-    })
+describe('MainPage component unit tests', () => {
+  test('Renders without crashing', () => {
+    const {getByText} = render(<MainPage />)
+
+    expect(getByText('Sand')).toBeInTheDocument()
+    expect(getByText('Add Item')).toBeInTheDocument()
+    expect(getByText('Coal')).toBeInTheDocument()
+    expect(getByText('Add Fuel')).toBeInTheDocument()
+    expect(getByText(MOCK_BUTTON_TEXT)).toBeInTheDocument()
+  })
+
+  test('Calls onClick prop when button clicked', () => {
+    const handleClick = jest.fn()
+
+    const {queryByText} = render(<MainPage onClick={handleClick()} />)
+    fireEvent.click(queryByText(MOCK_BUTTON_TEXT))
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  })
+
+  test('Buttons disabled if itemQuantity and fuelQuantity are empty', () => {
+    const {queryByText} = render(<MainPage />)
+
+    expect(queryByText('Add Item')).toBeDisabled();
+    expect(queryByText('Add Fuel')).toBeDisabled();
+  })
 })
